33a898792bc09676479b98b5946e04134fbcf0f2
Abdallh ELzayat <121009680+AbdallhElzayat2020@users.noreply.github.com>
2025-04-04 08:38:54 +0200

    Authenticated User shown

--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -52,8 +52,8 @@
                                     </div>
                                 </div>
                                 <div class="flex-grow-1">
-                                    <span class="fw-medium d-block">John Doe</span>
-                                    <small class="text-muted">Admin</small>
+                                    <span class="fw-medium d-block">{{auth()->guard('web')->user()->name}}</span>
+                                    <small class="text-muted">{{auth()->guard('web')->user()->email}}</small>
                                 </div>
                             </div>
                         </a>
